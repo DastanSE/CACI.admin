15e0de37ce8df4acb8fecbffa7621b22b699a186
Dastan <dastan.zhanibek@gmail.com>
2017-11-29 11:20:23 +0800

    test

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,6 +2,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  adminPassword: process.env.ADMIN_PASSWORD,
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
   cloudinaryApiSecretKey: process.env.CLOUDINARY_API_SECRET_KEY,
   cloudinaryUploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
